d36c3e0082543eeb4a355ac7a6249b10ef84f65f
Raphael Ainardi <ainardi.raphael@icloud.com>
2024-05-22 15:16:40 +0200

    fini

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :restaurant
+  resources :restaurants do
+    resources :reviews, only: [:index, :create]
+  end
+  resources :reviews, only: [:show, :edit, :update, :destroy]
 end
